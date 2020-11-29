49052c8f6a453351cf6427dad48491eb4b278857
Rizky Puji Raharja <rizky.rsj@gmail.com>
2020-11-29 22:32:50 +0700

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="bg-gray-100 h-screen"></div>
+  <div class="bg-gray-50 h-screen">
+    <div class="bg-white bordered border-b border-gray-200 p-2">
+      <div class="container">
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          class="mt-1"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
